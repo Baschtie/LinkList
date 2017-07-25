3b0d2636d172d1730618bd22859d928d4df02c38
Baschtie <Mr.Maccrafter@gmail.com>
2017-07-26 00:59:16 +0200

    add moment.js to improve time display

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,14 @@
 module LinksHelper
+
+  def display_expire_flag(link)
+    if !link[:does_expire]
+      return "<p>Never</p>".html_safe
+    else
+      if link[:experies_on] == Date.today
+        return "<div>Today</div>".html_safe
+      else
+        return "<div class=\"ui momentjs\" data-tooltip=\"#{link[:experies_on].strftime("%d.%m.%Y")}\" data-position=\"top left\">#{link[:experies_on].strftime("%Y%m%d")}</div>".html_safe
+      end
+    end
+  end
 end
