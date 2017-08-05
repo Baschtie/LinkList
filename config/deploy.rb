f32b36ffc824aa858762339cbe2b5c302ae5eebd
Baschtie <Baschtie@users.noreply.github.com>
2017-08-05 13:55:39 +0200

    lets step backwards here

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/Baschtie/LinkList.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/apps/LinksList"
+set :deploy_to, "/home/deploy/apps/LinkList"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
