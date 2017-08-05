9cac8787c21c1599e8a7afbb9373b91c9a83545f
Baschtie <Baschtie@users.noreply.github.com>
2017-08-05 13:04:31 +0200

    correctly use capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "3.9.0"
 
 set :application, "LinkList"
-set :repo_url, "https://github.com/Baschtie/LinkList/tree/master"
+set :repo_url, "https://github.com/Baschtie/LinkList.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/apps/LinksList"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
