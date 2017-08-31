3d3bd1dece14aa1db5e523032beec65caaf6c0e4
Baschtie <Baschtie@users.noreply.github.com>
2017-08-31 17:45:25 +0200

    improve flash-message handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,29 @@ module ApplicationHelper
     end
     return result
   end
+
+  def flash_class(key)
+    if key == "notice"
+      return "ui closable info message"
+    elsif key == "error"
+      return "ui closable floating negative message"
+    elsif key == "success"
+      return "ui closable floating positive message"
+    else
+      return "ui closable message"
+    end
+  end
+
+  def flash_header(key)
+    if key == "notice"
+      return "Info!"
+    elsif key == "error"
+      return "Error!"
+    elsif key == "success"
+      return "Success!"
+    else
+      return "Attention!"
+    end
+  end
+
 end
