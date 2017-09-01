6c79021ab24773af514c322ff88f404f5b2a005e
Baschtie <Baschtie@users.noreply.github.com>
2017-09-01 16:44:11 +0200

    uniqueness should only affect the same user

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ApplicationRecord
   validates :name, :presence => true
-  validates :url, :presence => true, :uniqueness => true
+  validates :url, :presence => true
+  validates :url, uniqueness: { scope: :belongs_to, :message => "Link is already stored" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20170726144835) do
     t.date     "experies_on"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["url"], name: "index_links_on_url", unique: true
   end
+
+  add_index :links, [:url, :belongs_to], unique: true
 
   create_table "tags", force: :cascade do |t|
     t.string   "name",        null: false
