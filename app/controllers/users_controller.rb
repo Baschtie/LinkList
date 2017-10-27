f9c89d9ee8b176f9b4402e42d86945c61910a869
Baschtie <Baschtie@users.noreply.github.com>
2017-10-27 09:51:17 +0200

    improve Account info page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,12 @@ class UsersController < ApplicationController
         raise ActionController::RoutingError.new('No Access')
       else
         @User = User.find_by_id(params[:id]) or raise ActionController::RoutingError.new('Not Found')
+        @link_count = Link.where(:belongs_to => params[:id]).count
       end
 
     else
       @User = User.find_by_id(current_user.id) or raise ActionController::RoutingError.new('Internal Server Error')
+      @link_count = Link.where(:belongs_to => params[:id]).count
     end
   end
 end
